077dbd42e169fe7ea53838d1a9e6db77b1eb9623
Joey Palchak <joey.palchak@gmail.com>
2023-08-21 10:30:52 -0700

    Test 1 passed.
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 // BUSINESS LOGIC for Places ---------------
+function Place(location, landmarks, dateVisited) {
+    this.location = location;
+    this.landmarks = landmarks;
+    this.dateVisited = dateVisited;
+}
