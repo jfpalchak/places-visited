9781333c45266245d4d992cc037e96a6753ed9c1
Joey Palchak <joey.palchak@gmail.com>
2023-08-21 14:33:05 -0700

    add UI logic section
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,22 @@ Place.prototype.location = function () {
     return this.city + ", " + this.state;
 }
 
-let pdx = new Place("Porland", "Oregon", ["Powell's Books", "Big Pink"], "8/21/2023");
+
+// UI LOGIC -----------------------------------
+// function runEverything() {
+
+//     const pdx = new Place("Porland", "Oregon", ["Powell's Books", "Big Pink"], "8/21/2023");
+//     const salem = new Place("Salem","Oregon", ["The Capitol", "In-N-Out"], "1/12/1999");
+//     const corvallis = new Place("Corvallis", "Oregon", ["OSU", "Mary's Peak", "Sky High"], "8/20/2023");
+
+//     const pdxContainer = document.getElementById("pdx-cover");
+//     const salemContainer = document.getElementById("salem-cover");
+//     const corvallisContainer = document.getElementById("corvallis-cover");
+
+
+    
+
+// }
+
+window.addEventListener("load", runEverything);
+    document.querySelector
